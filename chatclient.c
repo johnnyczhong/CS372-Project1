924dffe5556fdbb5c4acca153ed89877fe2771b3
Johnny Zhong <johnnyczhong@gmail.com>
2019-02-04 22:45:16 -0800

    following rubric

--- chatclient.c
+++ chatclient.c
@@ -1,5 +1,10 @@
 /*
-** client.c -- modeled after beej's guide to network programming
+ * Programmer Name: Johnny Zhong
+ * Program Name: chatclient.c
+ * Program Description: A chat client that communicates with a server.
+ * Course Name: CS372 - Introduction to Networks
+ * Last Modified: 2019-02-04
+** Citations: modeled after beej's guide to network programming
 */
 
 #include <stdio.h>
@@ -14,9 +19,8 @@
 
 #include <arpa/inet.h>
 
-#define PORT "12000" // the port client will be connecting to 
-
 #define MAXDATASIZE 1024 // max number of bytes we can get at once 
+
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
@@ -29,21 +33,83 @@ void *get_in_addr(struct sockaddr *sa)
 }
 
 
+// initiates the connection to the server
+void initiate_connection(int sockfd, char* username)
+{
+	char initial_msg[MAXDATASIZE];
+	sprintf(initial_msg, "%s%s", "Connecting to server. Username: ", username);
+	send(sockfd, initial_msg, strlen(initial_msg), 0);
+}
+
+
+// function to send messages. returns 1 if client initiated close
+int send_msg(int sockfd, char* username)
+{
+	char out_msg[MAXDATASIZE];
+	char out_buffer[MAXDATASIZE];
+
+	printf("Outgoing msg: ");
+	fgets(out_buffer, MAXDATASIZE, stdin);
+	out_buffer[strlen(out_buffer) - 1] = '\0'; // replace newline
+
+	// append handle to message
+	sprintf(out_msg, "%s> %s", username, out_buffer);
+
+	// send message to server
+	if (send(sockfd, out_msg, strlen(out_msg), 0) == -1) {
+		perror("send");
+		exit(1);
+	}
+
+	// detect if client initiated closing connection
+	if (strcmp(out_buffer, "\\quit") == 0) {
+		return 1;
+	}
+
+	return 0;
+
+}
+
+
+// function to receive messages. returns 1 if server requested to close
+int recv_msg(int sockfd)
+{
+	int numbytes;
+	char buf[MAXDATASIZE];
+
+	// receive message from server
+	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+	    perror("recv");
+	    exit(1);
+	}
+	buf[numbytes] = '\0';
+
+	// print server message to screen
+	printf("%s\n",buf);
+
+	// detect server closing connection
+	if (strcmp(buf, "Server> \\quit") == 0) {
+		return 1;
+	}
+
+	return 0;
+
+}
+
+
 int main(int argc, char *argv[])
 {
-	int sockfd, numbytes;  
-	char buf[MAXDATASIZE];
+	int sockfd;
 	struct addrinfo hints, *servinfo, *p;
 	int rv;
 	char s[INET6_ADDRSTRLEN];
 	char username[10];
 
-    char* out_buffer = calloc(MAXDATASIZE - 10, sizeof(char));
-    char* out_msg = calloc(MAXDATASIZE, sizeof(char));
+    // char* out_buffer = calloc(MAXDATASIZE - 10, sizeof(char));
+    // char* out_msg = calloc(MAXDATASIZE, sizeof(char));
 
-
-	if (argc != 2) {
-	    fprintf(stderr,"usage: client hostname\n");
+	if (argc != 3) {
+	    fprintf(stderr,"usage: server address and port number\n");
 	    exit(1);
 	}
 
@@ -51,7 +117,7 @@ int main(int argc, char *argv[])
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 
-	if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+	if ((rv = getaddrinfo(argv[1], argv[2], &hints, &servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 		return 1;
 	}
@@ -88,42 +154,18 @@ int main(int argc, char *argv[])
 	printf("Provide username: ");
 	fgets(username, 10, stdin);
 	username[strlen(username) - 1] = '\0'; // replace newline
-	send(sockfd, username, strlen(username), 0);
+
+	initiate_connection(sockfd, username);
 
 	do
 	{
-		// get user's message to send
-		printf("Outgoing msg: ");
-		fgets(out_buffer, MAXDATASIZE, stdin);
-		out_buffer[strlen(out_buffer) - 1] = '\0'; // replace newline
 
-		// append handle to message
-		sprintf(out_msg, "%s> %s", username, out_buffer);
-
-		// send message to server
-		if (send(sockfd, out_msg, strlen(out_msg), 0) == -1) {
-			perror("send");
-			exit(1);
-		}
-
-		// detect if client initiated closing connection
-		if (strcmp(out_buffer, "\\quit") == 0) {
+		if (send_msg(sockfd, username) == 1){
 			printf("Closing connection.\n");
 			break;
 		}
-
-		// receive message from server
-		if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
-		    perror("recv");
-		    exit(1);
-		}
-		buf[numbytes] = '\0';
-
-		// print server message to screen
-		printf("%s\n",buf);
-
-		// detect server closing connection
-		if (strcmp(buf, "Server> \\quit") == 0) {
+			
+		if (recv_msg(sockfd) == 1){
 			printf("Server closed connection.\n");
 			break;
 		}
--- chatserve.py
+++ chatserve.py
@@ -1,16 +1,56 @@
+'''
+Programmer Name: Johnny Zhong
+Program Name: chatserve.py
+Program Description: Starts a chat server for communication with a chat client.
+Course Name: CS372 - Introduction to Networks
+Last Modified: 2019-02-04
+Citations: CS372 14th Lecture Notes, Page 9
+'''
+
+import argparse
 import socket
 
 
-def main():
-	
-	SERVER_PORT = 12000
-	MSG_SIZE = 1024
-	SERVER_HANDLE = 'Server> '
+MSG_SIZE = 1024
+SERVER_HANDLE = 'Server> '
 
+
+def startup_server_socket(port):
 	# setting up socket
 	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server_socket.bind(('', SERVER_PORT))
+	server_socket.bind(('', port))
 	server_socket.listen(1)
+
+	return server_socket
+
+
+# 
+def receive_msg(connection_socket, client_handle):
+	client_msg = connection_socket.recv(MSG_SIZE)
+
+	# client requested quit
+	if client_msg == (client_handle + '> \\quit'):
+		return False
+	else:  # display client message
+		print client_msg
+		return True
+
+
+def send_msg(connection_socket):
+	# send message
+	message = raw_input('Message: ')
+	connection_socket.send(SERVER_HANDLE + message)
+
+	# server(self) requested quit
+	if message == '\\quit':
+		return False
+	else:
+		return True
+
+
+def main(port):
+
+	server_socket = startup_server_socket(port)
 
 	# awaiting connection loop
 	while True:
@@ -19,27 +59,22 @@ def main():
 
 		# accept new connection
 		connection_socket, address = server_socket.accept()
-		client_handle = connection_socket.recv(MSG_SIZE)
+		client_msg = connection_socket.recv(MSG_SIZE)
+		print client_msg
+
+		# parse client handle
+		client_handle = client_msg.split(': ')[1]
 
 		# communication loop
 		while True:
 			# receive message from client
-			client_msg = connection_socket.recv(MSG_SIZE)
-
-			# client requested quit
-			if client_msg == (client_handle + '> \\quit'):
-				print 'Client closed connection'
+			if not receive_msg(connection_socket, client_handle):
+				print 'Client closed connection.'
 				break
-			else:  # display client message
-				print client_msg
 
 			# send message
-			message = raw_input('Message: ')
-			connection_socket.send(SERVER_HANDLE + message)
-
-			# server(self) requested quit
-			if message == '\\quit':
-				print 'Closing connection'
+			if not send_msg(connection_socket):
+				print 'Closing connection.'
 				break
 
 		print 'server: Closing connection...'
@@ -47,4 +82,8 @@ def main():
 
 
 if __name__ == '__main__':
-	main()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('port', type=int, help='port to listen on for incoming connections')
+	args = parser.parse_args()
+
+	main(args.port)
